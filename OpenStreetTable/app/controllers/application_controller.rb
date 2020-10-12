479c2c7917f51e871fbc240e3083762b1590d93d
alvinC <lvinz0790@gmail.com>
2020-10-12 15:24:31 -0700

    CRLL done

--- OpenStreetTable/app/controllers/application_controller.rb
+++ OpenStreetTable/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in? 
+    #CRLL
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in? 
+        !!current_user
+    end
+
+    def require_logged_in!
+        render json: ['Invalid Credentials'] unless logged_in? 
+    end
+
+    def login!(user) 
+        session[:session_token] = user.reset_session_token! 
+    end
+
+    def logout! 
+        current_user.reset_session_token! 
+        session[:session_token] = nil 
+    end
+
+
 end
