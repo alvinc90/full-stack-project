3b23e69eaa0c10bbd4f9397e4a5284ddcac3c397
alvinC <lvinz0790@gmail.com>
2020-10-12 14:52:18 -0700

    added model level validation

--- OpenStreetTable/app/models/user.rb
+++ OpenStreetTable/app/models/user.rb
@@ -1,2 +1,41 @@
 class User < ApplicationRecord
-end
+    validates :username, :email, :password_digest, :session_token, presence: true
+    validates :username, :email, :session_token, uniqueness: true 
+    validates :password, length: {minimum: 4, allow_nil: true}
+
+    after_initialize :ensure_session_token
+    # SPIRE 
+
+    def password 
+        @password 
+    end
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token! 
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save! 
+        self.session_token
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password) 
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user 
+        user.is_password?(password) ? user : nil 
+    end
+
+    
+
+    private 
+
+    def ensure_session_token 
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+end     
