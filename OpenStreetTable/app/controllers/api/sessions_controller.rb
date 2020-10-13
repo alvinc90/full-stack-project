8d4329355e8fe38db869d9d0bcdf1164cc82bf28
alvinC <lvinz0790@gmail.com>
2020-10-12 21:44:44 -0700

    added ajax in util

--- OpenStreetTable/app/controllers/api/sessions_controller.rb
+++ OpenStreetTable/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,27 @@
 class Api::SessionsController < ApplicationController
+    before_action :require_logged_in!, only: [:destroy]
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user 
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ["Invalid Username"], status: 404
+        end
+    end
+
+    def destroy 
+        @user = current_user 
+        if @user 
+            logout! 
+            render "api/users/show"
+        else
+            render json: ["Not Signed In"], status: 404
+        end
+    end
 end
