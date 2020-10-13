18aba244b7b87f611b02a82c50a4ed58d73e13da
alvinC <lvinz0790@gmail.com>
2020-10-13 16:02:27 -0700

    added signup component

---
+++ OpenStreetTable/frontend/components/session/signup.jsx
@@ -0,0 +1,40 @@
+import React from 'react'; 
+
+class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.props.badAtNaming
+        this.handleSubmit = this.handleSubmit.bind(this) 
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.action(this.state)
+    }
+
+    handleChange(type) {
+        return (e) => {
+            this.setState({ [type]: e.currentTarget.value})
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.props.formType}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Username:
+                        <input type="text" value={this.state.username} onChange={this.handleChange('username')}/>
+                    </label>
+                    <label>email:
+                        <input type="text" value={this.state.email} onChange={this.handleChange('email')}/>
+                    </label>
+                    <label>Password:
+                        <input type="password" value={this.state.password} onChange={this.handleChange('password')}/>
+                    </label>
+                    <button>{this.props.formType}</button>
+                </form>
+            </div>
+        )
+    }
+}; 
---
+++ OpenStreetTable/frontend/components/session/signup_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux'; 
+import { signup } from '../../actions/session_actions';
+import Signup from './signup'; 
+
+const mapStateToProps = (state) => {
+    return ({
+        badAtNaming: {
+            username: "", 
+            email: "", 
+            password: ""
+        },
+        formType: "Sign Up"
+    })
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return ({
+        action: (formUser) => dispatch(signup(formUser))
+    })
+}; 
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signup); 
