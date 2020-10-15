b875f19a5f25aaaa03ed72162d6bf9a3f620af5b
alvinC <lvinz0790@gmail.com>
2020-10-15 15:08:22 -0700

    added 404 page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import {AuthRoute} from '../util/route_utils';
 import Modal from './modal/modal';  
-import NavBarContainer from './nav_bar/nav_bar_container'
+import NavBarContainer from './nav_bar/nav_bar_container';
+import Error from "./error";
 
 // import SignUpContainer from "./session/signup_container";
 // import LoginContainer from "./session/login_container"
@@ -11,11 +12,12 @@ const App = () => {
     return (
         <div>
             < Modal />
-            {/* <Switch> */}
-            <Route path="/" component={NavBarContainer}/>
+            <Switch>
+                <Route exact path="/" component={NavBarContainer}/>
                 {/* <Route path="/login" component={LoginContainer} />
                 <AuthRoute path="/signup" component={SignUpContainer} /> */}
-            {/* </Switch> */}
+                <Route component={Error}/>
+            </Switch>
 
         </div>
     )
---
+++ frontend/components/error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => (
+    <div className="error">
+        <h1>404 Error Page</h1>
+        <p>Couldn't find route</p>
+        <p>Sorry!! =(</p>
+    </div>
+);
+
+export default Error;
