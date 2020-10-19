66a866b6e02c8be5e66ef5cc813fd54f66f58954
alvinC <lvinz0790@gmail.com>
2020-10-19 10:54:27 -0700

    added window.url on app.html.erb

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <div className="food">
-                {/* <img className="food" src={window.foodURL} alt="food"/> */}
+            <div>
+                <img className="food" src={window.foodURL} alt="food"/>
             </div>
             <h1 className="home-heading">Find your table for any occasion</h1>
         </div>
