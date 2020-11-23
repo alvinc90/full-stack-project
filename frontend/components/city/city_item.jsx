7ee56b6fb6b81b71e59be4e7cc7e4ec89f859eaf
alvinC <lvinz0790@gmail.com>
2020-11-22 19:21:04 -0800

    changes

--- frontend/components/city/city_item.jsx
+++ frontend/components/city/city_item.jsx
@@ -15,33 +15,10 @@ const CityItem = ({city}) => {
     }
     return (
         <div>
-            <div><h1 className="city-heading">{city.name}</h1><img className="sf" src={cityImg} alt={city.name} /></div>
-            {/* <div className="outer-image-container"> */}
-
-                {/* <div className="inner-image-container"> */}
-                    {/* <div>  */}
-                        {/* { (city.name === "San Francisco") ? <div><h1 className="city-heading">{city.name}</h1><img className="sf" src={window.sfURL} alt="san francisco" /></div> : ""} */}
-                        {/* <div><h1 className="city-heading">{city.name}</h1><img className="sf" src={window.sfURL} alt="san francisco" /></div> */}
-                    {/* </div> */}
-                    {/* <div> */}
-                        {/* {(city.name === "New York") ? <div><h1 className="city-heading">{city.name}</h1><img className="ny" src={window.nyURL} alt="new york" /></div> : ""} */}
-                        {/* <div><h1 className="city-heading">{city.name}</h1><img className="ny" src={window.nyURL} alt="new york" /></div> */}
-                    {/* </div> */}
-                {/* </div> */}
-
-
-                {/* // <div className="inner-image-container"> */}
-                    {/* <div> */}
-                        {/* {(city.name === "Chicago") ? <div><h1 className="city-heading">{city.name}</h1><img className="chicago" src={window.chicagoURL} alt="chicago" /></div> : ""} */}
-                        {/* <div><h1 className="city-heading">{city.name}</h1><img className="chicago" src={window.chicagoURL} alt="chicago" /></div> */}
-                    {/* </div> */}
-                    {/* <div> */}
-                        {/* {(city.name === "Los Angeles") ? <div><h1 className="city-heading">{city.name}</h1><img className="la" src={window.laURL} alt="los angeles" /></div> : ""} */}
-                        {/* <div><h1 className="city-heading">{city.name}</h1><img className="la" src={window.laURL} alt="los angeles" /></div> */}
-                    {/* </div> */}
-                {/* // </div> */}
-
-            {/* // </div> */}
+            <div>
+                <h1 className="city-heading">{city.name}</h1>
+                <img className="sf" src={cityImg} alt={city.name} />
+            </div>
 
         </div>
 
