4ab6c7c505c625b3f1be0e913191df299f873ed3
alvinC <lvinz0790@gmail.com>
2020-11-24 13:31:59 -0800

    splash page styling

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -8,8 +8,10 @@ const Home = () => {
         <div>
             <div>
                 <img className="food" src={window.foodURL} alt="food" />
-                <h1 className="home-heading">Find your table for any occasion</h1>
-                <div className="home-search-bar"><HomeSearchBar /></div>
+                <div className="home-heading-container">
+                    <h1 className="home-heading">Find your table for any occasion</h1>
+                    <span className="home-search-bar-container"><HomeSearchBar /></span>
+                </div>
             </div>
 
             <div className="city-container3">
@@ -19,9 +21,20 @@ const Home = () => {
                 <CityContainer />
             </div>
             
-                
-            <footer>
-                <p className="footer">I am footer</p>
+            
+            <footer className="footer">
+                <div className="footer-container">
+                    <h6>Inspired by OpenTable</h6>
+                    <div className="alvin-links-container">
+                        <h6 className="my-name">Alvin Chong</h6>
+                        <div className="links">
+                            <a href="https://www.linkedin.com/in/alvin-chong-4b88011a1/" target="_blank"><img className="links-logo" src={window.liURL} alt="linkedIn"/></a>
+                        </div>
+                        <div className="links">
+                            <a href="https://github.com/alvinc90" target="_blank"><img className="links-logo" src={window.gitURL} alt="github"/></a>
+                        </div>
+                    </div>
+                </div>
             </footer>
 
         </div>
@@ -29,3 +42,5 @@ const Home = () => {
 }; 
  
 export default Home;
+
+
--- frontend/components/home/home_search_bar.jsx
+++ frontend/components/home/home_search_bar.jsx
@@ -6,35 +6,44 @@ class HomeSearchBar extends React.Component {
         return (
             <div>
                 <form>
-                    <input type="date"/>
-                    <input type="time"/>
-                    <select>
-                        <option value="">1 person</option>
-                        <option selected value="">2 people</option>
-                        <option value="">3 people</option>
-                        <option value="">4 people</option>
-                        <option value="">5 people</option>
-                        <option value="">6 people</option>
-                        <option value="">7 people</option>
-                        <option value="">8 people</option>
-                        <option value="">9 people</option>
-                        <option value="">10 people</option>
-                        <option value="">Larger Party</option>
-                    </select>
-                    <select>
-                        <option value="">All Cities</option>
-                        <option value="">San Francisco</option>
-                        <option value="">New York</option>
-                        <option value="">Chicago</option>
-                        <option value="">Los Angeles</option>
-                    </select>
-                    {/* <input type="search" placeholder="searchbar"/> */}
-                    <input type="submit" value="Let's go"/>
+                    <div className="search-bar-outer-container">
+                        
+                        <div className="date-time-people-container">
+                            <input type="date" className="search-bar-input-date"/>
+                            <input type="time" className="search-bar-input-time"/>
+                            <select className="search-bar-people-dropdown">
+                                <option value="">1 person</option>
+                                <option selected value="">2 people</option>
+                                <option value="">3 people</option>
+                                <option value="">4 people</option>
+                                <option value="">5 people</option>
+                                <option value="">6 people</option>
+                                <option value="">7 people</option>
+                                <option value="">8 people</option>
+                                <option value="">9 people</option>
+                                <option value="">10 people</option>
+                                <option value="">Larger Party</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <select className="search-bar-city-dropdown">
+                                <option value="">All Cities</option>
+                                <option value="">San Francisco</option>
+                                <option value="">New York</option>
+                                <option value="">Chicago</option>
+                                <option value="">Los Angeles</option>
+                            </select>
+                            <input className="search-bar-button" type="submit" value="Let's go"/>
+                        </div>
+
+                    </div>
                 </form>
             </div>
-
         )
     }
 };
 
 export default HomeSearchBar;
+
+
