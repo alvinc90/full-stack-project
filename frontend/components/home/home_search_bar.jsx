338c20fda862acb4b19bf4c2e2efd62033477f9c
alvinC <lvinz0790@gmail.com>
2020-11-23 20:03:43 -0800

    search bar cont..

--- frontend/components/home/home_search_bar.jsx
+++ frontend/components/home/home_search_bar.jsx
@@ -8,7 +8,27 @@ class HomeSearchBar extends React.Component {
                 <form>
                     <input type="date"/>
                     <input type="time"/>
-                    <input type="search" placeholder="searchbar"/>
+                    <select>
+                        <option value="">1 person</option>
+                        <option selected value="">2 people</option>
+                        <option value="">3 people</option>
+                        <option value="">4 people</option>
+                        <option value="">5 people</option>
+                        <option value="">6 people</option>
+                        <option value="">7 people</option>
+                        <option value="">8 people</option>
+                        <option value="">9 people</option>
+                        <option value="">10 people</option>
+                        <option value="">Larger Party</option>
+                    </select>
+                    <select>
+                        <option value="">All Cities</option>
+                        <option value="">San Francisco</option>
+                        <option value="">New York</option>
+                        <option value="">Chicago</option>
+                        <option value="">Los Angeles</option>
+                    </select>
+                    {/* <input type="search" placeholder="searchbar"/> */}
                     <input type="submit" value="Let's go"/>
                 </form>
             </div>
