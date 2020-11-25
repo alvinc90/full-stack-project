c05a20883fb61b22ccbf96747f7cc98e3dd65a29
alvinC <lvinz0790@gmail.com>
2020-11-25 09:30:02 -0800

    made index item 4 on each row

--- frontend/components/city/city_item.jsx
+++ frontend/components/city/city_item.jsx
@@ -14,7 +14,7 @@ const CityItem = ({city}) => {
     }
     return (
         <div>
-            <Link to={`/cities/${city.id}`}><div>
+            <Link to={`/cities/${city.id}`}><div className="cities-index-container">
                 <h1 className="city-heading">{city.name}</h1>
                 <img className="sf" src={cityImg} alt={city.name} />
             </div></Link>
