97bdd902affaea311ec141337d43b11b98aacd90
alvinC <lvinz0790@gmail.com>
2020-12-07 14:40:36 -0800

    update reservation functionality done

--- frontend/components/reservation/reservation_show.jsx
+++ frontend/components/reservation/reservation_show.jsx
@@ -52,7 +52,7 @@ class ReservationShow extends React.Component {
             // .then(this.props.history.push("/"));
         timeBtn.style.display = "none";
         inputS.style.display = "none";
-        editBtn.style.display = "block";
+        editBtn.style.display = "none";
         window.scrollTo(0, 0);
 
     }
@@ -102,6 +102,16 @@ class ReservationShow extends React.Component {
         setTimeout(this.handleLoaded2, 1400);
     }
 
+    generateRandomNum(length) {
+        let arr = [];
+        for(let i = 0; i <= length; i++) {
+            let rand = Math.floor( Math.random() * 9);
+            arr.push(rand);
+        }
+        let conNum = arr.join("")
+        return conNum;
+    }
+
     render() {
         const { reservation, restaurants, currentUser } = this.props;
         if (!Object.values(restaurants).length) return null;
@@ -129,9 +139,11 @@ class ReservationShow extends React.Component {
         const nowTime = reservation.time.slice(11, 16)
         const timeFormat = time1[nowTime];
         // debugger
-        return(
-            <div>
-                <div className="reservation-top-line"></div>
+        if (reservation.reserved) {
+            return(
+                <div>
+                    <div className="reservation-top-line"></div>
+    
                     <div className="reservation-show-button-container">
                         <Link to="/">
                             <button className="reservation-show-top-button">Home</button>
@@ -140,140 +152,153 @@ class ReservationShow extends React.Component {
                             <button className="reservation-show-top-button">My Reservation</button>
                         </Link>
                     </div>
-                <div className="reservation-show-outer-container">
-
-                    <div className="reservation-show-heading-container">
-                        <h1 className="rs1">Thanks! Your reservation is confirmed.</h1>
-                        <h2 className="rs2">Confirmation #some-randon-generated-num</h2>
+    
+                    <div className="reservation-show-outer-container">
+    
+                        <div className="reservation-show-heading-container">
+                            <h1 className="rs1">Thanks! Your reservation is confirmed.</h1>
+                            <h2 className="rs2">Confirmation #{this.generateRandomNum(6)}</h2>
+                            {/* <h2 className="rs2">Confirmation #some-randon-generated-num</h2> */}
+                        </div>
+    
+                        <div>
+                            <div className="reservation-show-main-container">
+                                <div>
+                                    <img 
+                                        src={restaurants[reservation.restaurant_id].photourl }
+                                        alt="restaurant picture"
+                                        className="reservation-image"
+                                    />
+                                </div>
+    
+                                <div>
+                                    <h1 className="rs3">{ restaurants[reservation.restaurant_id].name }</h1>
+                                    <div className="re4">
+                                        <div className="re500">
+                                            <span>{printDay}</span>
+                                            <span className="rx7">{printMonth}</span>
+                                            <span className="rx7">{printDate}</span>
+                                            <span className="rx7">{printYear}</span>
+                                        </div>
+                                        {/* <span>{ new Date(reservation.date).toDateString() }</span> */}
+                                        <div className="re501">
+                                            <span>{ timeFormat }</span>
+                                            <span className="rx8">{ reservation.num_guests } people</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+    
+                            <div className="re5">
+                                <p className="re6">{ reservation.special_request }</p>
+                            </div>
+    
+                            <div className="edit-form-button-container">
+                                <button onClick={this.handleModify} className="re7">Modify</button>
+                                <button onClick={} className="re8">Cancel Reservation</button>
+                            </div>
+    
+                            <div className="reservation-edit-form-outer-container">
+                                <div id="re256">
+                                    <div className="re100">
+                                        <div>
+                                            <select 
+                                                className="re12" 
+                                                defaultValue={reservation.num_guests}
+                                                onChange={this.handleChange('num_guests')}
+                                            >
+                                                <option value="1">1 person</option>
+                                                <option selected value="2">2 people</option>
+                                                <option value="3">3 people</option>
+                                                <option value="4">4 people</option>
+                                                <option value="5">5 people</option>
+                                                <option value="6">6 people</option>
+                                                <option value="7">7 people</option>
+                                                <option value="8">8 people</option>
+                                                <option value="9">9 people</option>
+                                                <option value="10">10 people</option>
+                                            </select>
+                                        </div>
+    
+                                        {/* value={reservation.time.slice(11, 16)} */}
+                                        <div>
+                                            <select 
+                                                className="re11" 
+                                                defaultValue= {reservation.time.slice(11, 16)}
+                                                onChange={this.handleChange('time')}
+                                            >
+                                                <option value="10:00">10:00 AM</option>
+                                                <option selected value="11:00">11:00 AM</option>
+                                                <option value="12:00">12:00 PM</option>
+                                                <option value="13:00">1:00 PM</option>
+                                                <option value="14:00">2:00 PM</option>
+                                                <option value="15:00">3:00 PM</option>
+                                                <option value="16:00">4:00 PM</option>
+                                                <option value="17:00">5:00 PM</option>
+                                                <option value="18:00">6:00 PM</option>
+                                                <option value="19:00">7:00 PM</option>
+                                                <option value="20:00">8:00 PM</option>
+                                                <option value="21:00">9:00 PM</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="re99">
+    
+                                        <div>
+                                            <input 
+                                                className="re10" 
+                                                type="date"
+                                                defaultValue={reservation.date}
+                                                onChange={this.handleChange('date')}
+                                            />
+                                        </div>
+                                        
+                                        <div>
+                                            <textarea 
+                                                className="re13"
+                                                placeholder="Add a special request(optional)"
+                                                onChange={this.handleChange('special_request')}
+                                            >
+                                            {reservation.special_request}
+                                            </textarea>
+                                        </div>
+                                    </div>
+    
+                                </div>
+    
+                                <div className="re37">
+                                    <button onClick={this.handleLoaded3} className="re50">Search</button>
+                                    <button onClick={this.handleCancel} className="re52">Cancel</button>
+                                </div>
+    
+                                <div id="sticky-loader2"></div>
+    
+                                <div className="ninety-9-ranch">
+                                    <button className="ranch99">Time1</button>
+                                    <button onClick={this.handleUpdate} className="ranch99">Time2</button>
+                                    <button className="ranch99">Time3</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <div>
-                        <div className="reservation-show-main-container">
-                            <div>
-                                <img 
-                                    src={restaurants[reservation.restaurant_id].photourl }
-                                    alt="restaurant picture"
-                                    className="reservation-image"
-                                />
-                            </div>
-
-                            <div>
-                                <h1 className="rs3">{ restaurants[reservation.restaurant_id].name }</h1>
-                                <div className="re4">
-                                    <div className="re500">
-                                        <span>{printDay}</span>
-                                        <span className="rx7">{printMonth}</span>
-                                        <span className="rx7">{printDate}</span>
-                                        <span className="rx7">{printYear}</span>
-                                    </div>
-                                    {/* <span>{ new Date(reservation.date).toDateString() }</span> */}
-                                    <div className="re501">
-                                        <span>{ timeFormat }</span>
-                                        <span className="rx8">{ reservation.num_guests } people</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="re5">
-                            <p className="re6">{ reservation.special_request }</p>
-                        </div>
-
-                        <div className="edit-form-button-container">
-                            <button onClick={this.handleModify} className="re7">Modify</button>
-                            <button className="re8">Cancel Reservation</button>
-                        </div>
-
-                        <div className="reservation-edit-form-outer-container">
-                            <div id="re256">
-                                <div className="re100">
-                                    <div>
-                                        <select 
-                                            className="re12" 
-                                            defaultValue={reservation.num_guests}
-                                            onChange={this.handleChange('num_guests')}
-                                        >
-                                            <option value="1">1 person</option>
-                                            <option selected value="2">2 people</option>
-                                            <option value="3">3 people</option>
-                                            <option value="4">4 people</option>
-                                            <option value="5">5 people</option>
-                                            <option value="6">6 people</option>
-                                            <option value="7">7 people</option>
-                                            <option value="8">8 people</option>
-                                            <option value="9">9 people</option>
-                                            <option value="10">10 people</option>
-                                        </select>
-                                    </div>
-
-                                    {/* value={reservation.time.slice(11, 16)} */}
-                                    <div>
-                                        <select 
-                                            className="re11" 
-                                            defaultValue= {reservation.time.slice(11, 16)}
-                                            onChange={this.handleChange('time')}
-                                        >
-                                            <option value="10:00">10:00 AM</option>
-                                            <option selected value="11:00">11:00 AM</option>
-                                            <option value="12:00">12:00 PM</option>
-                                            <option value="13:00">1:00 PM</option>
-                                            <option value="14:00">2:00 PM</option>
-                                            <option value="15:00">3:00 PM</option>
-                                            <option value="16:00">4:00 PM</option>
-                                            <option value="17:00">5:00 PM</option>
-                                            <option value="18:00">6:00 PM</option>
-                                            <option value="19:00">7:00 PM</option>
-                                            <option value="20:00">8:00 PM</option>
-                                            <option value="21:00">9:00 PM</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div className="re99">
-
-                                    <div>
-                                        <input 
-                                            className="re10" 
-                                            type="date"
-                                            defaultValue={reservation.date}
-                                            onChange={this.handleChange('date')}
-                                        />
-                                    </div>
-                                    
-                                    <div>
-                                        <textarea 
-                                            className="re13"
-                                            placeholder="Add a special request(optional)"
-                                            onChange={this.handleChange('special_request')}
-                                        >
-                                        {reservation.special_request}
-                                        </textarea>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className="re37">
-                                <button onClick={this.handleLoaded3} className="re50">Search</button>
-                                <button onClick={this.handleCancel} className="re52">Cancel</button>
-                            </div>
-
-                            <div id="sticky-loader2"></div>
-
-                            <div className="ninety-9-ranch">
-                                <button className="ranch99">Time1</button>
-                                <button onClick={this.handleUpdate} className="ranch99">Time2</button>
-                                <button className="ranch99">Time3</button>
-                            </div>
-                        </div>
-                    </div>
+    
+                    <footer>
+                        <Footer />
+                    </footer>
+    
                 </div>
-
-                <footer>
-                    <Footer />
-                </footer>
-
-            </div>
-        )
+            )  
+        }
+        else {
+            return(
+                <div>
+                    <div className="fart">I FART ON YOUR NOSE</div>
+                    <footer>
+                        <Footer />
+                    </footer>
+                </div>
+            )
+        }
     }
 };
 
