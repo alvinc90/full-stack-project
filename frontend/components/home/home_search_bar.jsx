7d23daf83b5d1e9027c969bcb2a53472ce86c815
alvinC <lvinz0790@gmail.com>
2020-12-08 14:33:59 -0800

    fix search bar alignment

--- frontend/components/home/home_search_bar.jsx
+++ frontend/components/home/home_search_bar.jsx
@@ -32,25 +32,37 @@ class HomeSearchBar extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <div className="search-bar-outer-container">
                         
-                        <div className="date-time-people-container">
+                        {/* <div className="date-time-people-container"> */}
                             <input type="date" className="search-bar-input-date"/>
-                            <input type="time" className="search-bar-input-time"/>
-                            <select className="search-bar-people-dropdown">
-                                <option value="">1 person</option>
-                                <option selected value="">2 people</option>
-                                <option value="">3 people</option>
-                                <option value="">4 people</option>
-                                <option value="">5 people</option>
-                                <option value="">6 people</option>
-                                <option value="">7 people</option>
-                                <option value="">8 people</option>
-                                <option value="">9 people</option>
-                                <option value="">10 people</option>
-                                <option value="">Larger Party</option>
+                            <select className="search-bar-input-time" defaultValue="11:00">
+                                <option value="10:00">10:00 AM</option>
+                                <option value="11:00">11:00 AM</option>
+                                <option value="12:00">12:00 PM</option>
+                                <option value="13:00">1:00 PM</option>
+                                <option value="14:00">2:00 PM</option>
+                                <option value="15:00">3:00 PM</option>
+                                <option value="16:00">4:00 PM</option>
+                                <option value="17:00">5:00 PM</option>
+                                <option value="18:00">6:00 PM</option>
+                                <option value="19:00">7:00 PM</option>
+                                <option value="20:00">8:00 PM</option>
+                                <option value="21:00">9:00 PM</option>
                             </select>
-                        </div>
-
-                        <div>
+                            <select className="search-bar-people-dropdown" defaultValue="2">
+                                <option value="1">1 person</option>
+                                <option value="2">2 people</option>
+                                <option value="3">3 people</option>
+                                <option value="4">4 people</option>
+                                <option value="5">5 people</option>
+                                <option value="6">6 people</option>
+                                <option value="7">7 people</option>
+                                <option value="8">8 people</option>
+                                <option value="9">9 people</option>
+                                <option value="10">10 people</option>
+                            </select>
+                        {/* </div> */}
+                            {/* <input type="date" className="search-bar-input-date"/> */}
+                        {/* <div> */}
                             <select onChange={this.handleSelectCity} className="search-bar-city-dropdown">
                                 <option value="All Cities" id="1">All Cities</option>
                                 <option value="San Francisco" id="2">San Francisco</option>
@@ -59,7 +71,7 @@ class HomeSearchBar extends React.Component {
                                 <option value="Los Angeles" id="4">Los Angeles</option>
                             </select>
                             <input className="search-bar-button" type="submit" value="Let's go"/>
-                        </div>
+                        {/* </div> */}
 
                     </div>
                 </form>
